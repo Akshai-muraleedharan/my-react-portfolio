64a8ba7a1fe2c38cf2ca59411f4d4e02b052b70b
Akshai Muraleedharan <akshaimuraleedharan4@gmail.com>
2024-04-17 15:04:20 +0530

    commit message

---
+++ src/Components/Header.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import "./Header.css"
+
+function Header() {
+    return (
+        <header>
+            
+                <h1 className='logo'>akshai <span className='letter-m'>M</span><span className='fullname'>uraleedharan</span></h1>
+                 <input type="checkbox" id='nav-toggle' className='nav-toggle' />
+                  
+              
+                <nav >
+                    <ul>
+                        <li>home</li>
+                        <li>about</li>
+                        <li>services</li>
+                        <li>contact</li>
+                    </ul>
+                </nav>
+
+                <label htmlFor="nav-toggle" className='nav-toggle-label'>
+                    <span></span>
+                  </label>
+            
+        </header>
+
+    )
+};
+
+export default Header;
---
+++ src/Components/Main.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Main.css"
+import { IconName } from "react-icons/";
+
+function Main() {
+    return(
+     <div className="container">
+        <section className="wrapper display-grid">
+          <div className="content-wrapper">
+            <h3>Hi there.</h3>
+            <h1>I'm Akshai Muraleedharan</h1>
+            <p>Fullstack web developer and UX/UI designer</p>
+          </div>
+          <div>photo</div>
+         
+        </section>
+     </div>
+    )
+}
+
+
+export default Main;
